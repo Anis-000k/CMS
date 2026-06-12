5e7c6ee68a3083888dcd3f34994225a6faae9a9f
Anis-000k <ar919008@gmail.com>
2026-06-12 14:00:39 +0530

    Use environment variables

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from re import search
 
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import os, Flask, render_template, request, redirect, session, flash
 from flask_mysqldb import MySQL
 import bcrypt
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Anis@264787'
-app.config['MYSQL_DB'] = 'cms'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
+
+app.secret_key = os.getenv('SECRET_KEY')
 
 mysql = MySQL(app)
 
