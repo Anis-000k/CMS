f105f5a80cf71f6e38a77f17a2971473fbf78bb2
Anis-000k <ar919008@gmail.com>
2026-06-12 14:11:32 +0530

    Fix env variables

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from re import search
-
-from flask import os, Flask, render_template, request, redirect, session, flash
+import os
+from flask import Flask, render_template, request, redirect, session, flash
 from flask_mysqldb import MySQL
 import bcrypt
-
 app = Flask(__name__)
+
+print("MYSQL_HOST =", os.getenv("MYSQL_HOST"))
+print("MYSQL_USER =", os.getenv("MYSQL_USER"))
+print("MYSQL_DB =", os.getenv("MYSQL_DB"))
+print("MYSQL_PORT =", os.getenv("MYSQL_PORT"))
+
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
-
-app.secret_key = os.getenv('SECRET_KEY')
 
 mysql = MySQL(app)
 
